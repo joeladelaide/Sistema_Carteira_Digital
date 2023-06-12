fdc6585ccc43c0760d0dd22f6662e51bb30fe6b4
Auto Logon <autologon@CSED.LAB>
2023-06-12 19:45:08 -0300

    feat: adiciona classe para criptoativo

--- src/Criptoativo.java
+++ src/Criptoativo.java
@@ -1,4 +1,11 @@
+package Programa;
 
-public class Criptoativo {
-
+public class Criptoativo extends Criptomoeda {
+	
+	private static int contador = 1;
+	
+	protected int idCriptoativo;
+	protected Criptomoeda criptomoeda;
+	protected Double quantidade = 0.0;
+	
 }
