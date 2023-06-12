ed295346f868bb5801015aeface8e3a101146d8b
AyrtonRZ <ayrtonrodrigues45@gmail.com>
2023-06-12 19:51:19 -0300

    feat: adiciona classe criptoativo e metodo para obter o id da criptomoeda

--- src/Criptoativo.java
+++ src/Criptoativo.java
@@ -8,4 +8,15 @@ public class Criptoativo extends Criptomoeda {
 	protected Criptomoeda criptomoeda;
 	protected Double quantidade = 0.0;
 	
+    public Criptoativo(Criptomoeda criptomoeda, String nome, String sigla, String exchange) {
+		super(nome, sigla, exchange);
+		this.idCriptoativo = contador;
+		this.criptomoeda = criptomoeda;
+		contador += 1;
+	}
+
+    // Getter para obter o identificador da criptomoeda
+	public int getIdCriptoativo() {
+		return idCriptoativo;
+	}
 }
